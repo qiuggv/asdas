2601c0b870d70ca7011489901ce02cc1074959c7
qiuggv <>
2019-10-29 09:53:54 +0800

    上传代码到分支[master], recordId=22

---
+++ src/main/java/com/myapp/sadfasd/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.myapp.sadfasd.mapper;
+
+import com.myapp.sadfasd.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/com/myapp/sadfasd/service/IUserService.java
@@ -0,0 +1,21 @@
+package com.myapp.sadfasd.service;
+
+import com.myapp.sadfasd.entity.User;
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+
+public interface IUserService {
+
+PageInfo<User> query(String username, int pageNum, int pageSize);
+
+    User selectById(Integer id);
+
+    int save(User user);
+
+    int update(User user);
+
+    int deleteById(Integer id);
+
+    int saveSafely(List<User> list);
+}
